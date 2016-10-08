fffe5e5f884605eac7c5d0c7f3378de85606e7d3
quantumke <nguruben@gmail.com>
2016-10-08 13:12:49 +0300

    added input layer

--- index.py
+++ index.py
@@ -20,6 +20,18 @@ class Encrypt_AI():
         NUM_EPOCHS = 60
         LEARNING_RATE = 0.0008
 
-   
+    def input_layer(input_, filter_shape, stride, name="input_layer"):
+        with tf.variabe_scope(name):
+            w=tf.get_variable('w'. shape=filter_shape,
+                              initializer=tf.contrib.layers.xavier_initializer())
+            conv=tf.nn.input_layer(input_,w,stride, padding="SAME")
+            return conv
+
+ 
+        
+        
+
+
+
 
 app=Encrypt_AI()
