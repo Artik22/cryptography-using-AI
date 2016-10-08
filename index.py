47880710d5210a1215d6ba3cbd7c9d93b256e4e8
quantumke <nguruben@gmail.com>
2016-10-08 13:12:05 +0300

    defined global keys

---
+++ index.py
@@ -0,0 +1,25 @@
+import tensoflow as tf
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class Encrypt_AI():
+    def __init__(self, *args, **kwargs):
+        self.author="Ben Nguru"
+    def __start__encrypt():
+        filters=[
+            [4, 1, 2],
+            [2, 2, 4],
+            [1, 4, 4],
+            [1, 4, 1]]
+        MSG_LEN = 16
+        KEY_LEN = 16
+        BATCH_SIZE = 512
+        NUM_EPOCHS = 60
+        LEARNING_RATE = 0.0008
+
+   
+
+app=Encrypt_AI()
