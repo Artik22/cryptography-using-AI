b228f388f442aa4c877146fb18b93cdd3a36a221
quantumke <nguruben@gmail.com>
2016-10-09 13:13:27 +0300

    build models to learn

--- index.py
+++ index.py
@@ -27,7 +27,23 @@ class Encrypt_AI():
             conv=tf.nn.input_layer(input_,w,stride, padding="SAME")
             return conv
 
- 
+    def conv_layer(hidden_layer_output, name):
+           h0 = tf.nn.relu(input_layer(hidden_layer_output, FILTERS[0], stride=1, name=name+'_h0_conv'))
+           h1 = tf.nn.relu(input_layer(h0, filters[1], stride=2, name=name+'_h1_conv'))
+           h2 = tf.nn.relu(input_layer(h1, filters[2], stride=1, name=name+'_h2_conv'))
+           h3 = tf.nn.tanh(input_layer(h2, filters[3], stride=1, name=name+'_h3_conv'))
+
+    def gen_data(n=BATCH_SIZE,msg_len=MSG_LEN,key_len=KEY_LEN):
+        return(np.random.randint(0,2,size=(n,msg_len))*2-1), \
+                                                             (np.random.randint(0,2,size=(n,key_len))*2-1)
+    def init_weights(name, shape):
+        return tf.get_variable(name, shape=shape,
+                               initializer=tf.contrib.layer, xavier_initializer())
+    def build_model():
+        self.w_one=init_weights("w_one",[2* self.N, 2*self.N])
+        self.w_one=init_weights("w_two",[2* self.N, 2*self.N])
+        self.w_one=init_weights("police_w1",[2* self.N, 2*self.N])
+        self.w_one=init_weights("police_w2",[2* self.N, 2*self.N])
         
         
 
